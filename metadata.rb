0f4e0dc6e2cff84af1ce355313c3f0055ca35604
Florian Asche <github@florian-asche.de>
2019-01-04 16:33:50 +0100

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -6,8 +6,6 @@ description 'Installs/Configures Elastic metricbeat'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '0.2.1'
 
-chef_version '~> 12'
-
 source_url 'https://github.com/vkhatri/chef-metricbeat' if respond_to?(:source_url)
 issues_url 'https://github.com/vkhatri/chef-metricbeat/issues' if respond_to?(:issues_url)
 
